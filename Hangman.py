4144ba6fe30afdbbabc9e48863aa8e8be1f82e01
Madhumitha <madhumithakolkar@gmail.com>
2024-02-25 17:30:58 +0530

    Finished Hangman

--- Hangman.py
+++ Hangman.py
@@ -4,50 +4,88 @@ import time
 # start = time.time()
 word_list = ["advark","baboon","camel"]
 
+stages = ['''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
+
 chosen_word = random.choice(word_list)
-length = len(chosen_word)
-chances = length
-word = ['_']*length
-
-# while chances>=1:
-#
-#     print("".join(word))
-#     guess = input("Guess a letter : ")
-#     guess = guess.lower()
-#
-#     letters = {}
-#     if guess in chosen_word:
-#         count = 0
-#         for index,ch in enumerate(chosen_word):
-#
-#             if ch == guess:
-#                 count += 1
-#                 letters[index] = ch
-#         chances -= count
-#         print("---",chances)
-#         print("Correct")
-#
-#         for index in letters:
-#             word[index] = letters[index]
-#
-#
-#     else:
-#
-#         chances -= 1
-#         print("Wrong")
-#         print(chances)
-#
-# if "_" in word:
-#     print("Game Over")
-# else:
-#     print("Win")
-# print(chosen_word)
-# # print("Time taken : ",(time.time()-start)*1000)
-
-guess = (input("Guess a letter : ")).lower()
 display = ['_']*len(chosen_word)
-print("The chosen word is : ", chosen_word)
-for index,char in enumerate(chosen_word):
-    if char == guess:
-        display[index] = guess
-print(display)
+chances = 6
+end_of_game = False
+
+print(f"Chances = {chances}")
+print(stages[chances])
+while not end_of_game and "_" in display:
+
+    guess = (input("Guess a letter : ")).lower()
+    same_char = 0
+
+    for index,char in enumerate(chosen_word):
+        if char == guess:
+            display[index] = guess
+
+    if guess not in chosen_word:
+        chances -= 1
+
+    print(f"Chances left = {chances}")
+    print(display)
+    if chances <= 0: end_of_game = True
+    print(stages[chances])
+
+if "_" in display:
+    print("Sorry but you lost !")
+else:
+    print("You win !")
