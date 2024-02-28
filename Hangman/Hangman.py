e8e1c1f48914810d381e0e352b35d5d8ce59b1a8
Madhumitha <madhumithakolkar@gmail.com>
2024-02-28 05:56:33 +0530

    Adding two new projects

--- Hangman/Hangman.py
+++ Hangman/Hangman.py
@@ -1,91 +1,40 @@
 import random
-import time
+import hangman_words
+import hangman_art
+import os
 
-# start = time.time()
-word_list = ["advark","baboon","camel"]
-stages = ['''
-  +---+
-  |   |
-  O   |
- /|\  |
- / \  |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
- /    |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|   |
-      |
-      |
-=========''', '''
-  +---+
-  |   |
-  O   |
-  |   |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
-      |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-      |
-      |
-      |
-      |
-=========
-''']
+word_list = hangman_words.word_list
+
+print(hangman_art.logo)
 
 chosen_word = random.choice(word_list)
-display = ['_']*len(chosen_word)
+display = ['_'] * len(chosen_word)
 chances = 6
 end_of_game = False
 
-print(f"Chances = {chances}")
-print(stages[chances])
+print(" ".join(display))
+print(hangman_art.stages[chances])
 while not end_of_game and "_" in display:
 
     guess = (input("Guess a letter : ")).lower()
-    same_char = 0
 
-    for index,char in enumerate(chosen_word):
+    for index, char in enumerate(chosen_word):
         if char == guess:
             display[index] = guess
 
-    if guess not in chosen_word:
+    if guess in display:
+        print(f"You've already guessed {guess}")
+    elif guess not in chosen_word:
+        print("You guessed a letter that's not in the word. You lose a life.")
         chances -= 1
 
-    print(f"Chances left = {chances}")
-    print("".join(display))
-    if chances <= 0: end_of_game = True
-    print(stages[chances])
+    print(" ".join(display))
+    if chances <= 0:
+        end_of_game = True
+    print(hangman_art.stages[chances])
 
 if "_" in display:
-    print("Sorry but you lost !")
+    print(f"Sorry but you lost !, the correct answer was {chosen_word}")
 else:
     print("You win !")
     end_of_game = True
