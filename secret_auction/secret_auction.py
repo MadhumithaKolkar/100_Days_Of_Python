851cac688cd71bd5df4337526be85da77cd5026b
Madhumitha <madhumithakolkar@gmail.com>
2024-03-02 05:39:34 +0530

    Adding secret Auction

--- secret_auction/secret_auction.py
+++ secret_auction/secret_auction.py
@@ -1,1 +1,15 @@
-#codes
+
+
+bid_values = {}
+make_bids = True
+
+while make_bids:
+    name = input("What is your name ? : ")
+    bid = int(input("What is your bidding price ? $ : "))
+    bid_values[bid] = name
+
+    more_bids = input("Type 'yes' if there are more people who want to bid, else type 'no'. : ")
+    make_bids = True if more_bids=="yes" else False
+
+max_bid = max(bid_values)
+print(f"The maximum bidder was {bid_values[max_bid]} with a bid value of ${max_bid}.")
