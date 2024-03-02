3fc1677adacc7f68b3ce491065bb8a419ed4bd4d
Madhumitha <madhumithakolkar@gmail.com>
2024-03-02 12:16:12 +0530

    Finished Secret Auction Project

--- secret_auction/secret_auction.py
+++ secret_auction/secret_auction.py
@@ -1,15 +1,22 @@
+import art
 
-
+print(art.logo)
 bid_values = {}
 make_bids = True
 
 while make_bids:
     name = input("What is your name ? : ")
     bid = int(input("What is your bidding price ? $ : "))
-    bid_values[bid] = name
+    bid_values[name] = bid
 
     more_bids = input("Type 'yes' if there are more people who want to bid, else type 'no'. : ")
     make_bids = True if more_bids=="yes" else False
 
-max_bid = max(bid_values)
-print(f"The maximum bidder was {bid_values[max_bid]} with a bid value of ${max_bid}.")
+max_bid = float("-inf")
+max_bidder = ""
+
+for names,bid in bid_values.items():
+    if bid>max_bid:
+        max_bid = bid
+        max_bidder = names
+print(f"The maximum bidder was {max_bidder} with a bid value of ${max_bid}.")
